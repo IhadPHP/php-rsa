8cc0540ceacc58240b9447947a1a1fdf1ffa1331
沈唁 <52o@qq52o.cn>
2019-01-04 20:53:09 +0800

    Create Aes.php

---
+++ src/Aes.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * PHP AES加密解密算法
+ * author: ShenYan.
+ * Email：52o@qq52o.cn
+ * CreatedTime: 2019/1/4 18:56
+ */
+
+namespace syrecords;
+
+class Aes
+{
+    public static function encrypt($input, $key) {
+        $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
+        $input = Aes::pkcs5_pad($input, $size);
+        $td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
+        $iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        mcrypt_generic_init($td, $key, $iv);
+        $data = mcrypt_generic($td, $input);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        $data = base64_encode($data);
+        return $data;
+    }
+
+    private static function pkcs5_pad ($text, $blocksize) {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+    public static function decrypt($sStr, $sKey) {
+        $decrypted= mcrypt_decrypt(
+            MCRYPT_RIJNDAEL_128,
+            $sKey,
+            base64_decode($sStr),
+            MCRYPT_MODE_ECB
+        );
+        $dec_s = strlen($decrypted);
+        $padding = ord($decrypted[$dec_s-1]);
+        $decrypted = substr($decrypted, 0, -$padding);
+        return $decrypted;
+    }
+    
+}
